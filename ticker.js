d92afb1b26585bb5392399f7b8a6f0aed40215fe
mhubenthal <hubentmx@gmail.com>
2014-05-22 00:30:06 -0400

    added rough proof of concept for cyclical animation

--- ticker.js
+++ ticker.js
@@ -35,5 +35,55 @@ tkr.VERSION = '0.0.1';
 tkr.helloWorld = function(name){
   console.log("Hello " + name + "\'s world!");
 };
+    
+// Nasty proof of concept
 
+tkr.test = function() {
+  var draw = function(){
+    var canvas = document.getElementById('canvas');
+    var bodyWidth = 350;
+    var bodyHeight = 200;
+
+    if (canvas.getContext) {
+      document.getElementById('canvas').width = 350;
+      document.getElementById('canvas').height = 200;
+      document.getElementById('canvas').border = "1px solid black";
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect(0,0,350,200);
+      // Draw vertical grid
+      for(var x=0;x<=bodyWidth;x++){
+        ctx.beginPath();
+        ctx.moveTo(x,0);
+        ctx.lineTo(x,bodyHeight);
+        ctx.closePath();
+        ctx.fillStyle="grey";
+        ctx.stroke();
+        x+=10;
+      }
+        
+      // Draw horizontal grid
+      for(var y=0;y<=bodyHeight;y++){
+        ctx.beginPath();
+        ctx.moveTo(0,y);
+        ctx.lineTo(bodyWidth,y);
+        ctx.closePath();
+        ctx.fillStyle="grey";
+        ctx.stroke();
+        y+=10;
+      }
+    }
+  };
+  draw();
+  var xPos = 300;
+  var drawCube = function(){
+    if(xPos<0){
+      xPos=300;
+    };
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "black";
+    ctx.fillRect(xPos,100,10,10);
+    xPos -= 10;
+  };
+  var run = setInterval(function(){draw(); drawCube();},200);
+};
 }.call(this));
