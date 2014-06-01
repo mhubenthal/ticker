f835463955da83b5ef3c29da6e801b1ba5485cf6
mhubenthal <hubentmx@gmail.com>
2014-05-31 22:44:03 -0400

    removed development code, add in remaining UTF-8 codes

--- ticker.js
+++ ticker.js
@@ -17,8 +17,8 @@
   /////////////////////////////////////////////
 
   // References to <canvas> element
-  // Note: for tkr to work, <canvas id="tkr_canvas"></canvas> 
-  //   must be included in the <body> tag of the document
+  // Note: for tkr to work, set the id of the <canvas>
+  //   element to "tkr_canvas".
   var tkr_canvas = document.getElementById("tkr_canvas");
   var tkr_ctx = tkr_canvas.getContext('2d');
 
@@ -31,30 +31,31 @@
   // Default tkr run status values
   var tkr_IntervalId, tkr_isPaused = false, tkr_isForward = false, tkr_isReversed = false;
     
-  // Array of tkr char shapes for UTF-8 codes
-  //  in decimal from 32 to 90.
+  // JSON object of tkr char shapes for UTF-8 codes
+  //  in decimal from 32 to 126.
   //  Here is a good list of the available chars:
   //    http://utf8-chartable.de/unicode-utf8-table.pl?utf8=dec
-  // Position [0] of each array is the required width of the shape
+  // Position [0] of each "pattern" is the width of the shape.
+  // Lowercase letters default to uppercase
     
     // \ !" so far
   var tkr_JSONcharShapes = {"charPatterns": [
       {"char": " ", "pattern": [3]},
-      {"char": "!", "pattern": [2,0,6,12,18,30]},
-      {"char": "\"", "pattern": [4,0,6,12,2,8,14]},
-      {"char": "#", "pattern": [6,1,3,6,7,8,9,10,13,15,19,21,24,25,26,27,28,31,33]},
-      {"char": "$", "pattern": [5]},
-      {"char": "%", "pattern": [5]},
-      {"char": "&", "pattern": [5]},
-      {"char": "'", "pattern": [2,0,6,12]},
+      {"char": "!", "pattern": [2,6,12,18,30]},
+      {"char": "\"", "pattern": [4,6,12,8,14]},
+      {"char": "#", "pattern": [6,7,9,12,13,14,15,16,19,21,24,25,26,27,28,31,33]},
+      {"char": "$", "pattern": [4,1,6,7,8,12,18,19,20,26,30,31,32,37]},
+      {"char": "%", "pattern": [5,12,15,20,25,30,33]},
+      {"char": "&", "pattern": [4,13,18,19,20,25]},
+      {"char": "'", "pattern": [2,6,12]},
       {"char": "(", "pattern": [4,2,7,12,18,25,32]},
       {"char": ")", "pattern": [4,0,7,14,20,25,30]},
-      {"char": "*", "pattern": [5]},
+      {"char": "*", "pattern": [6,6,8,10,13,14,15,18,19,20,21,22,25,26,27,30,32,34]},
       {"char": "+", "pattern": [4,13,18,19,20,25]},
       {"char": ",", "pattern": [2,30,36]},
       {"char": "-", "pattern": [4,18,19,20]},
       {"char": ".", "pattern": [2,30]},
-      {"char": "/", "pattern": [5]},
+      {"char": "/", "pattern": [5,15,20,25,30]},
       {"char": "0", "pattern": [4,6,7,8,12,14,18,20,24,26,30,31,32]},
       {"char": "1", "pattern": [2,6,12,18,24,30]},
       {"char": "2", "pattern": [4,6,7,8,14,18,19,20,24,30,31,32]},
@@ -67,11 +68,11 @@
       {"char": "9", "pattern": [4,6,7,8,12,14,18,19,20,26,30,31,32]},
       {"char": ":", "pattern": [2,18,30]},
       {"char": ";", "pattern": [2,18,30,36]},
-      {"char": "<", "pattern": [5]},
+      {"char": "<", "pattern": [3,13,18,25]},
       {"char": "=", "pattern": [4,12,13,14,24,25,26]},
-      {"char": ">", "pattern": [5]},
-      {"char": "?", "pattern": [5]},
-      {"char": "@", "pattern": [5]},
+      {"char": ">", "pattern": [3,12,19,24]},
+      {"char": "?", "pattern": [4,0,1,2,8,13,14,19,31]},
+      {"char": "@", "pattern": [6,0,1,2,3,4,6,10,12,14,16,18,20,21,22,24,30,31,32,33,34]},
       {"char": "A", "pattern": [4,6,7,8,12,14,18,19,20,24,26,30,32]},
       {"char": "B", "pattern": [4,6,7,8,12,14,18,19,20,24,26,30,31,32]},
       {"char": "C", "pattern": [4,6,7,8,12,18,24,30,31,32]},
@@ -80,8 +81,8 @@
       {"char": "F", "pattern": [4,6,7,8,12,18,19,20,24,30]},
       {"char": "G", "pattern": [4,6,7,8,12,18,20,24,26,30,31,32]},
       {"char": "H", "pattern": [4,6,8,12,14,18,19,20,24,26,30,32]},
-      {"char": "I", "pattern": [2,6,18,24,30]},
-      {"char": "J", "pattern": [4,8,20,24,26,30,31,32]},
+      {"char": "I", "pattern": [2,6,12,18,24,30]},
+      {"char": "J", "pattern": [4,8,14,20,24,26,30,31,32]},
       {"char": "K", "pattern": [4,6,8,12,14,18,19,24,26,30,32]},
       {"char": "L", "pattern": [4,6,12,18,24,30,31,32]},
       {"char": "M", "pattern": [6,6,7,8,9,10,12,14,16,18,20,22,24,26,28,30,32,34]},
@@ -97,7 +98,43 @@
       {"char": "W", "pattern": [6,6,8,10,12,14,16,18,20,22,24,26,28,30,31,32,33,34]},
       {"char": "X", "pattern": [6,6,10,13,15,20,25,27,30,34]},
       {"char": "Y", "pattern": [4,6,8,12,14,19,25,31]},
-      {"char": "Z", "pattern": [4,6,7,8,14,19,24,30,31,32]}
+      {"char": "Z", "pattern": [4,6,7,8,14,19,24,30,31,32]},
+      {"char": "[", "pattern": [3,6,7,12,18,24,30,31]},
+      {"char": "\\", "pattern": [5,12,19,26,33]},
+      {"char": "]", "pattern": [3,6,7,13,19,25,30,31]},
+      {"char": "^", "pattern": [4,7,12,14]},
+      {"char": "_", "pattern": [5,30,31,32,33]},
+      {"char": "`", "pattern": [3,6,13]},
+      {"char": "A", "pattern": [4,6,7,8,12,14,18,19,20,24,26,30,32]},
+      {"char": "B", "pattern": [4,6,7,8,12,14,18,19,20,24,26,30,31,32]},
+      {"char": "C", "pattern": [4,6,7,8,12,18,24,30,31,32]},
+      {"char": "D", "pattern": [4,8,14,18,19,20,24,26,30,31,32]},
+      {"char": "E", "pattern": [4,6,7,8,12,18,19,20,24,30,31,32]},
+      {"char": "F", "pattern": [4,6,7,8,12,18,19,20,24,30]},
+      {"char": "G", "pattern": [4,6,7,8,12,18,20,24,26,30,31,32]},
+      {"char": "H", "pattern": [4,6,8,12,14,18,19,20,24,26,30,32]},
+      {"char": "I", "pattern": [2,6,12,18,24,30]},
+      {"char": "J", "pattern": [4,8,14,20,24,26,30,31,32]},
+      {"char": "K", "pattern": [4,6,8,12,14,18,19,24,26,30,32]},
+      {"char": "L", "pattern": [4,6,12,18,24,30,31,32]},
+      {"char": "M", "pattern": [6,6,7,8,9,10,12,14,16,18,20,22,24,26,28,30,32,34]},
+      {"char": "N", "pattern": [5,6,9,12,13,15,18,20,21,24,27,30,33]},
+      {"char": "O", "pattern": [4,6,7,8,12,14,18,20,24,26,30,31,32]},
+      {"char": "P", "pattern": [4,6,7,8,12,14,18,19,20,24,30]},
+      {"char": "Q", "pattern": [4,6,7,8,12,14,18,19,20,26,32]},
+      {"char": "R", "pattern": [4,6,7,8,12,14,18,19,24,26,30,32]},
+      {"char": "S", "pattern": [4,6,7,8,12,18,19,20,26,30,31,32]},
+      {"char": "T", "pattern": [4,6,7,8,13,19,25,31]},
+      {"char": "U", "pattern": [4,6,8,12,14,18,20,24,26,30,31,32]},
+      {"char": "V", "pattern": [4,6,8,12,14,18,20,25,31]},
+      {"char": "W", "pattern": [6,6,8,10,12,14,16,18,20,22,24,26,28,30,31,32,33,34]},
+      {"char": "X", "pattern": [6,6,10,13,15,20,25,27,30,34]},
+      {"char": "Y", "pattern": [4,6,8,12,14,19,25,31]},
+      {"char": "Z", "pattern": [4,6,7,8,14,19,24,30,31,32]},
+      {"char": "{", "pattern": [4,7,8,13,18,25,31,32]},
+      {"char": "|", "pattern": [2,6,12,18,24,30]},
+      {"char": "}", "pattern": [4,6,7,13,20,25,30,31]},
+      {"char": "~", "pattern": [5,18,13,20,15]}
   ]};
 
   /////////////////////////////////////////////
@@ -332,12 +369,9 @@
   };
 
   /*** ADD IN MORE FUN CONTROLS/SUPRISES ***/ 
-
   // SAMPLE CODE TO TEST LIBRARY REVISIONS
   // Create sample shapes
-  tkr.setMessage(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  //tkr.setMessage("++ HELLO WORLD, WELCOME TO TICKER.JS ++ AND WELCOME TO MAXHUBENTHAL.COM ++");
-  console.log(tkr_message);
+  //tkr.setMessage(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`{|}~");
 
   // Register the tkr object to the global namespace
   this.tkr = tkr;
